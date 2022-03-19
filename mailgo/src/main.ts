2c9e06f31ab8771c6d6bbc263420befc6941aff4
Luismkm <luismarcelo13@yahoo.com.br>
2022-03-19 20:06:29 -0300

    feat: add badrequest interceptor

--- mailgo/src/main.ts
+++ mailgo/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestInterceptor } from './interceptors/badRequest.interceptor';
 import { UnauthorizedInterceptor } from './interceptors/unauthorized.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new UnauthorizedInterceptor());
+  app.useGlobalInterceptors(new BadRequestInterceptor());
 
   await app.listen(3000);
 }
