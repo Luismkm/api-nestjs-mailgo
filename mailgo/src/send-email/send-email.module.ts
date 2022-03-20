b5c5b6d00965980cb71b956e2feaa3d5e1ccb474
Luismkm <luismarcelo13@yahoo.com.br>
2022-03-20 20:33:48 -0300

    refactor: move RabbitMQModule imports to sendEmailModule and add providers

--- mailgo/src/send-email/send-email.module.ts
+++ mailgo/src/send-email/send-email.module.ts
@@ -1,19 +1,27 @@
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { Module } from '@nestjs/common';
-import { ClientTokenService } from 'src/client-token/client-token.service';
-import { ClientService } from 'src/client/client.service';
-import { WrapperModule } from 'src/rabbitmq/wrapper.module';
-import { UnsubscribeService } from 'src/unsubscribe/unsubscribe.service';
+import { ClientTokenModule } from 'src/client-token/client-token.module';
+import { ClientModule } from 'src/client/client.module';
+import { UnsubscribeModule } from 'src/unsubscribe/unsubscribe.module';
 import { SendEmailController } from './send-email.controller';
 import { SendEmailService } from './send-email.service';
 
 @Module({
-  imports: [WrapperModule],
-  controllers: [SendEmailController],
-  providers: [
-    SendEmailService,
-    UnsubscribeService,
-    ClientService,
-    ClientTokenService,
+  imports: [
+    RabbitMQModule.forRoot(RabbitMQModule, {
+      uri: `amqp://admin:admin@rabbitmq:5672`,
+      exchanges: [
+        {
+          name: 'exchange-mailgo',
+          type: 'direct',
+        },
+      ],
+    }),
+    ClientTokenModule,
+    UnsubscribeModule,
+    ClientModule,
   ],
+  controllers: [SendEmailController],
+  providers: [SendEmailService],
 })
 export class SendEmailModule {}
