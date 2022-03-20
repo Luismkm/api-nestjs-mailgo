34d78e7cc601dc5cccdc0f87e82f3b374d316b67
Luismkm <luismarcelo13@yahoo.com.br>
2022-03-20 20:31:53 -0300

    refactor: move RabbitMQModule imports to appModule

--- mailgo-consumer/src/app.module.ts
+++ mailgo-consumer/src/app.module.ts
@@ -1,16 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { WrapperModule } from './rabbitmq/wrapper.module';
 import { MailModule } from './mail/mail.module';
 import { ConfigModule } from '@nestjs/config';
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    WrapperModule,
+    RabbitMQModule.forRoot(RabbitMQModule, {
+      uri: `amqp://admin:admin@rabbitmq:5672`,
+      prefetchCount: 1,
+      exchanges: [
+        {
+          name: 'exchange-mailgo',
+          type: 'direct',
+        },
+      ],
+    }),
     MailModule,
   ],
   controllers: [AppController],
